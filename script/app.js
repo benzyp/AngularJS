8f2a57e725ba10d29b8de07bd75c00cf5eda578d
benzyp <benzyp@yahoo.com>
2018-11-09 14:23:36 -0500

    basing all on one selected value
    clearing out both boxes and center panel when deselecting

--- script/app.js
+++ script/app.js
@@ -36,7 +36,6 @@ myApp.controller('statisticsController', ['$scope', '$log', '$filter', '$http', 
     //this method changes the states2 as per the values in the type ahead filter box (typeaheadtext)
     $scope.typeAhead = function(){
         $scope.states2 = $scope.states.data.filter(function(typeAhead){
-            //console.log("begings with"+typeAhead.state.startsWith($scope.typeaheadtext));
             return typeAhead.state.toLowerCase().includes($scope.typeaheadtext.toLowerCase());
         });
         //console.log($scope.states2);
@@ -44,26 +43,17 @@ myApp.controller('statisticsController', ['$scope', '$log', '$filter', '$http', 
 
     $scope.stateDetails = function(selectedValue){
         $scope.selected = selectedValue; //set the current selection (this handles both boxes)
-        //this keeps the selection of both boxes in sync
-        $scope.selection1 = selectedValue;
-        $scope.selection2 = selectedValue;
-        //console.log($scope.selection2)
-        //reset the values of the county totals
+
         $scope.countyTotal = 0;
         $scope.countyTotalAccurate = false;
-        //console.log("value of selectedValue" + selectedValue);
-        //console.log($scope.selected);
-        //console.log(selectedValue[0].detail);
 
         //make an http call to get the state county data for the currently selected state
         $http.get(selectedValue[0].detail).
             success(function (result){
-                //console.log(result);
                 var countylist = [];
                 result.data.forEach(function(element) {
                     countylist.push(element.county + " " + element.population + '\r\n');
                     $scope.countyTotal+=element.population;
-                    //console.log("element "+element.county);
                   });
                   $scope.selectedCounties = countylist;
                   if($scope.countyTotal === selectedValue[0].population)
@@ -73,15 +63,10 @@ myApp.controller('statisticsController', ['$scope', '$log', '$filter', '$http', 
             }); 
     }
 
-    $scope.clearSelection = function(selectedBox){
-        if(selectedBox==='selection1'){
-            $scope.selection1 = {};          
-        }
-        if(selectedBox==='selection2'){
-            $scope.selection2 = {};
-        }
-        if($scope.selection1 === {} && $scope.selection2 === {}){
-        }
+    $scope.clearSelection = function(){
+        $scope.selected = {};
+        $scope.countyTotal = 0;
+        $scope.selectedCounties = {};
+        $scope.countyTotalAccurate = false;
     }
-
 }]);
